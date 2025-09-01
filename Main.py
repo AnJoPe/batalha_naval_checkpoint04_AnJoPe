37bb4c7b6bdaf76c65e3471c57cc9474a67478a8
pmiranda27 <pmiranda.senai29@gmail.com>
2025-09-01 10:51:53 -0300

    Feat: Renderização dos Navios na Matriz.

--- Main.py
+++ Main.py
@@ -2,31 +2,31 @@
 # PEQUENO = 0 4x4
 # MÉDIO = 1 5x5
 # GRANDE = 2 6x6
-tamanho_mapa = 0
+tamanho_mapa = 1
 
 matriz_partida = []
 if tamanho_mapa == 0:
     matriz_partida = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
+        [1, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
     ]
 elif tamanho_mapa == 1:
     matriz_partida = [
+        [0, 0, 1, 0, 0],
+        [1, 0, 0, 1, 0],
         [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
+        [0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0]
     ]
 elif tamanho_mapa == 2:
     matriz_partida = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
         [0, 0, 0, 0, 0, 0]
     ]
 
@@ -41,13 +41,26 @@ def desenhar_mapa():
     numero_linhas = len(matriz_partida)
     for linha in range(numero_linhas):
         for quadrado_coluna in range(numero_colunas):
-            matriz_desenhada += "|￣￣￣| "
+            matriz_desenhada += "|￣￣￣￣| "
         matriz_desenhada += "\n"
+
         for segunda_parede_quadrado in range(numero_colunas):
-            matriz_desenhada += "|     | "
+            if segunda_parede_quadrado >= numero_colunas / 2:
+                matriz_desenhada += " "
+            #print(f"VALOR MATRIZ: {matriz_partida[linha][segunda_parede_quadrado]}")
+            if matriz_partida[linha][segunda_parede_quadrado] == 1:
+                matriz_desenhada += "|  🚢   | "
+            else:
+                matriz_desenhada += "|       | "
+        matriz_desenhada += "\n"
+
+        for terceira_parede_quadrado in range(numero_colunas):
+            if terceira_parede_quadrado >= numero_colunas / 2:
+                matriz_desenhada += " "
+            matriz_desenhada += "|       | "
         matriz_desenhada += "\n"
     for quadrado_coluna in range(numero_colunas):
-        matriz_desenhada += " ￣￣￣  "
+        matriz_desenhada += " ￣￣￣￣  "
 
     print(matriz_desenhada)
 
